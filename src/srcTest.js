10cf585b65bd364c0773e2328fe5e49b93378b5a
ValentinFilipov <valentinmf@gmail.com>
2020-12-01 16:47:31 +0200

    sometings change

--- src/srcTest.js
+++ src/srcTest.js
@@ -1,6 +1,6 @@
 function addNumber (num1, num2) {
-    let summ = num1 + num2;
-    console.log(`Summ: ${num1} + ${num2} = ${summ}`);
+    let sum = num1 + num2;
+    console.log(`Sum: ${num1} + ${num2} = ${summ}`);
 }
 
 addNumber(166, 774);
